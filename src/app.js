f560f36cdd52ecdd590c823291ae3b0cac37687f
mulleragustin <agustinmuller98@gmail.com>
2023-02-24 19:39:37 -0300

    dirname

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import express from "express";
 import productsRouter from "./routes/products.router.js";
+import __dirname from "./utils.js";
+
 const app = express();
 
-app.use("/api/products", productsRouter)
+app.use(express.static(__dirname + "/../public"));
+
+app.use("/api/products", productsRouter);
 
 app.listen(8080, () => {
   console.log("Server listening on port 8080");
