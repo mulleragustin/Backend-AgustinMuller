5cdc11c7caa3fc7b570ca4327a264de5f2a89ffe
mulleragustin <agustinmuller98@gmail.com>
2023-02-28 23:33:46 -0300

    .

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cartsRouter from "./routes/carts.router.js";
 import productsRouter from "./routes/products.router.js";
 import __dirname from "./utils.js";
+import {engine} from "express-handlebars"
 
 const app = express();
 
@@ -10,6 +11,14 @@ app.use(express.static(__dirname + "/../public"));
 app.use("/api/products", productsRouter);
 
 app.use("/api/carts", cartsRouter);
+
+app.engine("handlebars",engine());
+app.set("view engine","handlebars");
+app.set("views",__dirname + "/views")
+
+app.get("/",(req,res) =>{
+  res.render('home')
+})
 
 app.listen(8080, () => {
   console.log("Server listening on port 8080");
